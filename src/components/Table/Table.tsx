f72484a65a1a4a2c8767f02a241653419e16b935
jean dominguez <jeansi123@live.com>
2025-02-21 23:56:19 -0500

    feat: new table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,10 +3,17 @@ import { JSX } from "react";
 interface TableProps<T extends { id: string }> {
   renderItems: (item: T) => JSX.Element;
   data: T[];
+  renderHeader?: () => JSX.Element;
 }
 export const Table = <T extends { id: string }>({
   renderItems,
+  renderHeader,
   data,
 }: TableProps<T>) => {
-  return <div className="w-full">{data.map((item) => renderItems(item))}</div>;
+  return (
+    <div className="w-full">
+      {renderHeader?.()}
+      {data.map((item) => renderItems(item))}
+    </div>
+  );
 };
